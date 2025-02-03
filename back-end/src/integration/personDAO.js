ceac56b64cd74e205e78ae584336ccd99be2e97a
thepoppe <pontus.morin@hotmail.se>
2025-02-04 00:02:25 +0100

    create Person added and info on how to connect to db

--- back-end/src/integration/personDAO.js
+++ back-end/src/integration/personDAO.js
@@ -3,3 +3,23 @@
  * finduserbyuser
  * creatuser
  */
+
+/* This should be in a config file*/
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+
+//try connection
+try {
+    await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+};
+
+/*file starts here*/
+const Person = require("../models/personModel");
+class personDAO {
+    async create(personDTO){
+        return await Person.create(personDTO)
+    }
+}
