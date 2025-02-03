3393234aa6b51353c1ee19558b8c69524e943568
thepoppe <pontus.morin@hotmail.se>
2025-02-04 00:01:30 +0100

    sequelize model in same structure as db

--- back-end/src/models/personModel.js
+++ back-end/src/models/personModel.js
@@ -1,3 +1,37 @@
 /**
  * PersonModel - interface for sequlize
  */
+
+
+const {Sequelize, DataTypes, Model} = require('sequelize');
+
+class Person extends Model {}
+
+Person.init(
+    {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        surname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        pnr: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+        {
+        sequelize, //unsure if we need this or if its only for local
+        modelName: 'person',
+    },
+);
