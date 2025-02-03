c96dd4a17b53c0cccfb65fd31ca329fcc077684c
thepoppe <pontus.morin@hotmail.se>
2025-02-04 00:01:07 +0100

    DTO matching with db structure

--- back-end/src/models/personDTO.js
+++ back-end/src/models/personDTO.js
@@ -1,3 +1,15 @@
 /**
  * The class that is treturned to the user
  */
+
+class PersonDTO {
+    constructor(json_person) {
+        this.name = json_person.name || ""; 
+        this.surname = json_person.surname || "";
+        this.pnr = json_person.pnr || "";
+        this.email = json_person.email || "";
+        this.username = json_person.username || "";
+    }
+}
+
+module.exports = PersonDTO;
