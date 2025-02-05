945943d7a8acf4e4e432e65dadac36395a8716c1
Zia <ziamohseni2013@gmail.com>
2025-02-05 18:12:16 +0100

    Fix navigation to be fix on top and fix the content overlapping

--- front-end/src/app/layout.js
+++ front-end/src/app/layout.js
@@ -13,8 +13,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navigation />
-        <main className="h-[calc(100vh-5rem)] flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 bg-gray-50">
+        {/* Fixed Navigation */}
+        <div className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
+          <Navigation />
+        </div>
+
+        {/* Main Content with Padding to Avoid Overlap */}
+        <main className="pt-[5rem] flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 bg-gray-50 min-h-screen">
           {children}
         </main>
       </body>
