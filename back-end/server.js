c8a63254aee114fa1cc3b340f698db88b9f218bf
thepoppe <pontus.morin@hotmail.se>
2025-02-05 20:22:35 +0100

    Pool removed, db checks connection and syncs before server starts

--- back-end/server.js
+++ back-end/server.js
@@ -1,31 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-const { Pool } = require("pg");
+const { syncDatabase } = require("./src/config/databaseConfig");; 
 
 const app = express();
 const port = 4000;
 
-// PostgreSQL connection setup
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
 
-// Test database connection
-app.get("/test-db", async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query("SELECT NOW() AS current_time");
-    client.release();
-    res.json({ success: true, time: result.rows[0].current_time });
-  } catch (err) {
-    console.error("Database connection error:", err);
-    res.status(500).json({ success: false, error: err.message });
-  }
-});
+
 
 // Root endpoint
 app.get("/", (req, res) => {
@@ -33,6 +14,17 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+const startServer = async () =>{
+  try {
+    await syncDatabase();
+    app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+    });
+  } 
+  catch (error) {
+          console.error("Server setup error:", error);
+          process.exit(1);
+      }
+};
+startServer();
+
