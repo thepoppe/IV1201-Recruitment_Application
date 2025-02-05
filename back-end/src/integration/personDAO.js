927bdd5f04dec02792aa322ddb3611845347c405
thepoppe <pontus.morin@hotmail.se>
2025-02-05 20:20:05 +0100

    db setup removed and first db methods created

--- back-end/src/integration/personDAO.js
+++ back-end/src/integration/personDAO.js
@@ -4,22 +4,20 @@
  * creatuser
  */
 
-/* This should be in a config file*/
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
-
-//try connection
-try {
-    await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-};
 
 /*file starts here*/
-const Person = require("../models/personModel");
-class personDAO {
-    async create(personDTO){
+const Person = require("../models/personModel")
+class PersonDAO {
+    async createPerson(personDTO){
         return await Person.create(personDTO)
     }
+    async findPersonByID(id){
+        const person = await Person.findAll({
+            where: {
+              person_id:id
+            }
+        })
+        return person;
+    }
 }
+module.exports = PersonDAO
