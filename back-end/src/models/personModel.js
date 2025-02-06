d753e11974b5061aaccb4127aa25b3c892ca2937
thepoppe <pontus.morin@hotmail.se>
2025-02-06 17:42:53 +0100

    password and simple validation added, hashing NOT added

--- back-end/src/models/personModel.js
+++ back-end/src/models/personModel.js
@@ -17,29 +17,63 @@ Person.init(
         name: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                is: /^[a-zA-Z]+$/,
+            },
         },
         surname: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                is: /^[a-zA-Z]+$/,
+            },
         },
         pnr: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true,
+            validate: {
+                is: /^[0-9]{8}-[0-9]{4}$/,
+            },
         },
         email: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING(64),
+            allowNull: false,
+            validate: {
+                len: [8, 64],
+                is: /^[0-9a-zA-Z]+$/i,
+                containsUpperLowerAndNumber(value) {
+                    if (!/[a-z]/.test(value) || !/[A-Z]/.test(value) || !/[0-9]/.test(value)) {
+                        throw new Error('Password must contain uppercase, lowercase letters, and numbers');
+                    }
+                }
+            },
         },
         username: {
             type: DataTypes.STRING,
-            allowNull: false,
         },
     },
-        {
+    {
         sequelize,
         modelName: 'Person',
         tableName: "person",
         timestamps: false,
+        hooks: {
+            beforeCreate: (person) => {
+                person.username = person.name + person.surname;
+            },
+            beforeUpdate: (person) => {
+                person.username = person.name + person.surname;
+            },
+        },
     },
 );
 
