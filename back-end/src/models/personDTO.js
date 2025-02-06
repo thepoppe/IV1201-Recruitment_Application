31b79afd5eac6cef212add14769a5faf62e7925a
thepoppe <pontus.morin@hotmail.se>
2025-02-06 16:56:52 +0100

    id field added to dto

--- back-end/src/models/personDTO.js
+++ back-end/src/models/personDTO.js
@@ -4,10 +4,11 @@
 
 class PersonDTO {
     constructor(json_person) {
-        this.name = json_person.name || ""; 
-        this.surname = json_person.surname || "";
-        this.pnr = json_person.pnr || "";
-        this.email = json_person.email || "";
+        this.id = json_person.person_id;
+        this.name = json_person.name; 
+        this.surname = json_person.surname;
+        this.pnr = json_person.pnr;
+        this.email = json_person.email;
         this.username = this.name+this.surname;
     }
 }
