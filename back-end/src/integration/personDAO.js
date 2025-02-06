ef02d32b176a1e529c686a0b06126538a974375f
thepoppe <pontus.morin@hotmail.se>
2025-02-06 20:40:27 +0100

    try catch clause and comments added

--- back-end/src/integration/personDAO.js
+++ back-end/src/integration/personDAO.js
@@ -9,26 +9,87 @@
 const Person = require("../models/personModel")
 const personDTO = require("../models/personDTO")
 class PersonDAO {
-    async createPerson(person){
-         const user = await Person.create(person);
 
-         return  new personDTO(user); 
+    /**
+    * Creates a new Person, saves it to the database, and returns a PersonDTO
+    * @param {Object} person - The person object containing name, surname, pnr, email, and password
+    * @param {string} person.name - The name of the person
+    * @param {string} person.surname - The surname of the person
+    * @param {string} person.pnr - The personal number of the person
+    * @param {string} person.email - The email of the person
+    * @param {string} person.password - The password of the person
+    * @returns {Promise<Object>} - The created Person Object
+    * @throws {Error} - If the person could not be created
+    */
+    async createPerson(person) {
+        try {
+            const user = await Person.create(person);
+            return new PersonDTO(user);
+        } catch (error) {
+            console.error("Error creating person:", error);
+            throw error;
+        }
     }
-    async findPersonByID(id){
-        const user = await Person.findOne({
-            where: {
-              person_id:id
+
+    /**
+    * Finds a Person by ID and returns a PersonDTO
+    * @param {number} id - The ID of the person
+    * @returns {Promise<PersonDTO>} - The found PersonDTO
+    * @throws {Error} - If the database query fails
+    */
+    async findPersonByID(id) {
+        try {
+            const user = await Person.findOne({
+                where: {
+                    person_id: id
+                }
+            });
+            return user ? new PersonDTO(user) : null;
+        } catch (error) {
+            console.error("Error finding person by ID:", error);
+            throw error;
+        }
+    }
+
+
+    /**
+    * Finds a Person by Name and returns an array of PersonDTOs
+    * @param {string} name - The name of the person
+    * @returns {Promise<Array<PersonDTO>>} - The found array of PersonDTOs
+    * @throws {Error} - If the database query fails
+    */
+    async findPersonByName(name) {
+        try {
+            const users = await Person.findAll({
+                where: {
+                    name: name
+                }
+            });
+            return users.length > 0 ? users.map(user => new PersonDTO(user)) : [];
+        } catch (error) {
+            console.error("Error finding person by name:", error);
+            throw error;
+        }
+    }
+
+        /**
+    * Finds a Person by PNR and returns an array of PersonDTOs
+    * @param {string} pnr - The personal number of the person
+    * @returns {Promise<Array<PersonDTO>>} - The found array of PersonDTOs
+    * @throws {Error} - If the database query fails
+    */
+        async findPersonByPNR(pnr) {
+            try {
+                const users = await Person.findAll({
+                    where: {
+                        pnr: pnr
+                    }
+                });
+                return users.length > 0 ? users.map(user => new PersonDTO(user)) : [];
+            } catch (error) {
+                console.error("Error finding person by PNR:", error);
+                throw error;
             }
-        })
-        return new personDTO(user);
-    }
-    async findPersonByName(name){
-        const users = await Person.findAll({
-            where: {
-              name:name
-            }
-        })
-        return users.map(user => new personDTO(user));
-    }
+        }
 }
 module.exports = PersonDAO
