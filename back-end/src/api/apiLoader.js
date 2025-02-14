62ae8219b9ca1f8676cf391d88f4a3744176fb3e
Zia <ziamohseni2013@gmail.com>
2025-02-14 09:52:54 +0100

    Move /api to apiLoader to be centralized

--- back-end/src/api/apiLoader.js
+++ back-end/src/api/apiLoader.js
@@ -8,7 +8,7 @@ class ApiLoader {
   async loadApis(app) {
     this.apis.forEach((api) => {
       api.registerRoutes();
-      app.use(api.basePath, api.router);
+      app.use(`/api${api.basePath}`, api.router);
     });
   }
 }
