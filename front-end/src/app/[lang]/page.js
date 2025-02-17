52e11e076719a2095cb10ed4ac971e1655f58172
Zia <ziamohseni2013@gmail.com>
2025-02-17 22:14:52 +0100

    Add translations in home page

--- front-end/src/app/[lang]/page.js
+++ front-end/src/app/[lang]/page.js
@@ -1,21 +1,19 @@
+"use client";
+import { useLanguage } from "../context/LanguageContext";
 import Button from "@/components/ui/Button";
 
 export default function Home() {
+  const { dict, lang } = useLanguage();
+
   const handleClick = () => {
     alert("Application form is not available yet");
   };
 
   return (
     <div className="flex flex-col items-center justify-center text-center">
-      <h1 className="text-4xl font-bold mb-6">
-        Welcome to Amusement Park Recruitment!
-      </h1>
-      <p className="max-w-lg text-lg mb-6">
-        Join our team and be part of the excitement! We’re looking for
-        passionate individuals to help create unforgettable experiences for our
-        visitors. Apply today and start your journey with us! 🚀🎢
-      </p>
-      <Button>Apply now</Button>
+      <h1 className="text-4xl font-bold mb-6">{dict.home.title}</h1>
+      <p className="max-w-lg text-lg mb-6">{dict.home.description} 🚀🎢</p>
+      <Button>{dict.home.apply}</Button>
     </div>
   );
 }
