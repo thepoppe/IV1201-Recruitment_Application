4cb74b6574d42ba2b61bcfc44d6c4422673f8a14
Zia <ziamohseni2013@gmail.com>
2025-02-18 17:17:13 +0100

    Create language switcher and add it to nav

--- front-end/src/components/Navigation.js
+++ front-end/src/components/Navigation.js
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { useLanguage } from "@/contexts/LanguageContext";
 import Button from "@/components/ui/Button";
+import LanguageSwitcher from "@/components/LanguageSwitcher";
 
 export default function Navigation() {
   const { dict, lang } = useLanguage(); // Get `dict` and `lang` from context
@@ -12,9 +13,11 @@ export default function Navigation() {
         <div className="flex justify-between h-16 items-center">
           <div className="flex-shrink-0">
             {/* Text logo link to home */}
-            <Link href={`/${lang}`} className="text-xl font-bold">
+            <Link href={`/${lang}`} className="text-xl font-bold mr-4">
               {dict.navigation.logo}
             </Link>
+            {/* Language Switcher */}
+            <LanguageSwitcher />
           </div>
           <div className="flex gap-4">
             {/* Login button */}
